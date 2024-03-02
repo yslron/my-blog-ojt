9e0119335ba5277018933a8c4f95396f69d1763e
Ais1e <yslron1000@gmail.com>
2024-03-02 14:03:14 +0800

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[600px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <div className="flex flex-col items-center justify-center gap-6">
-          <p className="text-2xl">I'm Ysl Ron</p>
+          <p className="text-2xl">I&apos;m Ysl Ron</p>
           <div className="flex gap-10">
             <h1 className="text-7xl">A UI/UX</h1>
             <h1 className="text-7xl font-semibold">DESIGNER</h1>
